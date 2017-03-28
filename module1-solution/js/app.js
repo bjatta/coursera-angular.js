993bfc476543b02a21300820e67b3d5d8f302655
Victor <me@bjatta.ru>
2017-03-28 12:04:43 +0300

    module1-bonus-done

--- module1-solution/js/app.js
+++ module1-solution/js/app.js
@@ -7,39 +7,48 @@
 	function feedEnough($scope){
 		$scope.foodList = 'apple, some eggs, coffee';
 		$scope.resultOfLunchMenuCheck = '';
-		$scope.lunchArray = [];
-		$scope.emptyDish  = [];
+		$scope.lunchArray  = [];
+		$scope.emptyDish   = [];
+		$scope.lunchResult = '';
+		$scope.dishResult  = '';
 		
 		$scope.solveFoodList = function () {
 		$scope.lunchArray.length = 0;
 		$scope.emptyDish.length  = 0;
-			$scope.foodList.split(',').forEach(
-				function(el){
-					if (el.length)
-						$scope.lunchArray.push(el);
-					else
-						$scope.emptyDish.push(el);
-				});
-			$scope.resultOfLunchMenuCheck = $scope.lunchArray.length;
+		$scope.foodList.split(',').forEach(
+			function(el){
+				var tempWord = el.replace(/^ | $|  /g,'');
+				if (tempWord.length)
+					$scope.lunchArray.push(tempWord);
+				else
+					$scope.emptyDish.push(tempWord);
+			});
+		$scope.resultOfLunchMenuCheck = $scope.lunchArray.length;
 		}
 		
 		$scope.resultToWord = function (){
-			if (!$scope.resultOfLunchMenuCheck) {return;}
 			switch (true){
 				case (1 >  $scope.resultOfLunchMenuCheck): {
+					$scope.lunchResult = 'alert alert-warning';
 					return "Too hungry for this! ";}
 				case (3 >  $scope.resultOfLunchMenuCheck): {
+					$scope.lunchResult = 'alert alert-info';
 					return "Enjoy! But U can more";}
 				case (3 == $scope.resultOfLunchMenuCheck): {
+					$scope.lunchResult = 'alert alert-success';
 					return "Enjoy!";}
-				default: {return "Too much";}
+				case (true): {
+					$scope.lunchResult= 'alert alert-danger'
+					return "Too much";}
 			}
 		}
 
 		$scope.emptyDishWarning = function(){
-			if ($scope.emptyDish.length && $scope.lunchArray.length) {
+			if ($scope.emptyDish.length) {
+				$scope.dishResult  = 'alert alert-danger';
 				return 'Why U serve ' + $scope.emptyDish.length + ' empty Dish?';
 			}
+			$scope.dishResult  = '';
 			return '';
 		}
 	}
