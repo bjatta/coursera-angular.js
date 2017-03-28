83ce55352d7909cd1339a5700d54d05a6cce7b90
Victor <me@bjatta.ru>
2017-03-28 11:13:21 +0300

    module1-before-bonus

--- module1-solution/js/app.js
+++ module1-solution/js/app.js
@@ -7,9 +7,40 @@
 	function feedEnough($scope){
 		$scope.foodList = 'apple, some eggs, coffee';
 		$scope.resultOfLunchMenuCheck = '';
+		$scope.lunchArray = [];
+		$scope.emptyDish  = [];
+		
 		$scope.solveFoodList = function () {
-			$scope.lunchArray = $scope.foodList.split(',');
+		$scope.lunchArray.length = 0;
+		$scope.emptyDish.length  = 0;
+			$scope.foodList.split(',').forEach(
+				function(el){
+					if (el.length)
+						$scope.lunchArray.push(el);
+					else
+						$scope.emptyDish.push(el);
+				});
 			$scope.resultOfLunchMenuCheck = $scope.lunchArray.length;
+		}
+		
+		$scope.resultToWord = function (){
+			if (!$scope.resultOfLunchMenuCheck) {return;}
+			switch (true){
+				case (1 >  $scope.resultOfLunchMenuCheck): {
+					return "Too hungry for this! ";}
+				case (3 >  $scope.resultOfLunchMenuCheck): {
+					return "Enjoy! But U can more";}
+				case (3 == $scope.resultOfLunchMenuCheck): {
+					return "Enjoy!";}
+				default: {return "Too much";}
+			}
+		}
+
+		$scope.emptyDishWarning = function(){
+			if ($scope.emptyDish.length && $scope.lunchArray.length) {
+				return 'Why U serve ' + $scope.emptyDish.length + ' empty Dish?';
+			}
+			return '';
 		}
 	}
 }
