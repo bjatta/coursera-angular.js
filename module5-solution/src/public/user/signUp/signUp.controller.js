193bd3711e11875abe65f444ad3844695170b4e6
Victor <me@bjatta.ru>
2017-05-01 08:53:01 +0300

    sign Up form pattern fix

--- module5-solution/src/public/user/signUp/signUp.controller.js
+++ module5-solution/src/public/user/signUp/signUp.controller.js
@@ -13,18 +13,18 @@ function SignUpController(MenuService) {
             firstName:'Jonh',
             lastName:'Doe',
             email:'John@Doe.sky',
-            phone:'555-222-777',
+            phone:'555-222-7777',
             favoratedDish:'SP4',
         };
     console.log($ctrl.user);
     $ctrl.submit = function() {
-        console.log('on submit:',$ctrl.user);
         MenuService.getMenuItemsByShortNames($ctrl.user.favoratedDish).then(function (response) {
             $ctrl.user.dish = response.data;
             MenuService.setUserInfo($ctrl.user);
             $ctrl.success = true;
             $ctrl.error = false;
         }, function (response) {
+            MenuService.setUserInfo({});
             console.log('error');
             $ctrl.success = false;
             $ctrl.error = true;
